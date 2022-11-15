e003695f959a1647d914defc28fe322aeb8caea4
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-15 11:22:46 -0300

    feat: configurando env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { AppDataSource } from './data-source';
 import { Accounts } from './entity/Accounts';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 AppDataSource.initialize()
   .then(async () => {
