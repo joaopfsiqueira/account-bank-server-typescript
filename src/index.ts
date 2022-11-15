3af6e6f20573e90639917745035e6b4db2072d2d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-15 11:15:59 -0300

    feat: ajustando index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { AppDataSource } from './data-source';
-import { User } from './entity/Accounts';
+import { Accounts } from './entity/Accounts';
 
 AppDataSource.initialize()
   .then(async () => {
     console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.age = 25;
-    await AppDataSource.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
+
+    const account = new Accounts();
+    account.balance = 100;
+
+    await AppDataSource.manager.save(account);
+    console.log('Saved a new user with id: ' + account.id);
 
     console.log('Loading users from the database...');
-    const users = await AppDataSource.manager.find(User);
+    const users = await AppDataSource.manager.find(Accounts);
     console.log('Loaded users: ', users);
 
     console.log(
