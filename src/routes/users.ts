8365e3e04197d9b1873907086c8112bb4ba46f57
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-17 00:22:19 -0300

    feat: ajustando router usuario

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'; //roteia as rotas, informa que tem algo acessando e ve se a rota que ele está tentando acessar existe.
 
-const routerUsuario = Router();
+export const routerUsuario = Router();
 
 /**
  * Criando uma rota padrão
