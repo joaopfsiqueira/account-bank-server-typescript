7dd2bbf07b413bed8b05b37bd68b15990b3a21e0
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-17 00:18:38 -0300

    feat: ajustando value em transactions

--- src/entity/Transactions.ts
+++ src/entity/Transactions.ts
@@ -8,16 +8,23 @@ import {
   ManyToOne,
 } from 'typeorm';
 import { Accounts } from './Accounts';
+import { ColumnNumericTransformer } from '../common/transformersNumerics';
 
 @Entity()
 export class Transactions {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0,
+    transformer: new ColumnNumericTransformer(),
+  })
   value: number;
 
-  @CreateDateColumn({ type: 'timestamptz', precision: -3 })
+  @CreateDateColumn()
   createdAt: Date;
 
   @ManyToOne(() => Accounts)
