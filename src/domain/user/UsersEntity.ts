849775e9e6ea5e3d0c51eb4b605e91e21e627b63
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-18 20:54:47 -0300

    feat: renomeando user entity dentor de domain

---
+++ src/domain/user/UsersEntity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  Unique,
+} from 'typeorm';
+import { Accounts } from '../account/AccountsEntity';
+
+@Entity()
+export class Users {
+  constructor(username: string, password: string, account: Accounts) {
+    this.username = username;
+    this.password = password;
+    this.account = account;
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @OneToOne(() => Accounts)
+  @JoinColumn()
+  account: Accounts;
+}
