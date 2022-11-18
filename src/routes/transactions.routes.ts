7c046f3e7e3a4bf6ffedbabeb70335f8739b5035
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-17 23:24:38 -0300

    feat: adicionando rotas do transactions

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'; //roteia as rotas, informa que tem algo acessando e ve se a rota que ele está tentando acessar existe.
+import { TransactionsController } from '../controller/TransactionsController';
+
+//exportando rota para utilizar no server.
+export const transactionRouter = Router();
+
+//instânciando controllers
+const transactionController = new TransactionsController();
+/**
+ * Serviço para salvar um novo usuario.
+ */
+
+transactionRouter.post('/', transactionController.create);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { app } from './app';
 import { userRouter } from './routes/users.routes';
+import { transactionRouter } from './routes/transactions.routes';
 
 app.use('/usuarios', userRouter);
+app.use('/transactions', transactionRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`App ouvindo na porta ${process.env.PORT}`)
