243617fed14614f736aa238206db7513dc41a7e7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 00:40:23 -0300

    feat: mudando transactions entity

---
+++ src/domain/transaction/TransactionsEntity.ts
@@ -0,0 +1,47 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Accounts } from '../account/AccountsEntity';
+import { ColumnNumericTransformer } from '../../common/transformersNumerics';
+
+@Entity()
+export class Transactions {
+  constructor(
+    value: number,
+    debitedAccount: Accounts,
+    creditedUsernameAccount: Accounts
+  ) {
+    this.value = value;
+    this.debitedAccount = debitedAccount;
+    this.creditedAccount = creditedUsernameAccount;
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0,
+    transformer: new ColumnNumericTransformer(),
+  })
+  value: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => Accounts)
+  @JoinColumn()
+  debitedAccount: Accounts;
+
+  @ManyToOne(() => Accounts)
+  @JoinColumn()
+  creditedAccount: Accounts;
+}
