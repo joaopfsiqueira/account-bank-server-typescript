9677270daad5c091c187644ff0d0f17c3eadd2a2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 01:17:57 -0300

    feat: checando se não é a mesma conta

--- src/domain/transaction/TransactionsController.ts
+++ src/domain/transaction/TransactionsController.ts
@@ -14,17 +14,23 @@ export class TransactionsController {
     const balance = new CheckBalance();
     const haveBalance = await balance.CheckUserBalance(debitedAccount, value);
     if (haveBalance === true) {
-      if (creditedUsernameAccount) {
-        const newTransaction = await transactionService.createTransaction(
-          value,
-          debitedAccount,
-          creditedUsernameAccount
-        );
+      if (creditedUsernameAccount.account.id != debitedAccount) {
+        if (creditedUsernameAccount) {
+          const newTransaction = await transactionService.createTransaction(
+            value,
+            debitedAccount,
+            creditedUsernameAccount
+          );
 
-        return res.send(newTransaction);
+          return res.send(newTransaction);
+        } else {
+          return res.status(400).send({
+            Message: 'Usuário não localizado!',
+          });
+        }
       } else {
         return res.status(400).send({
-          Message: 'Usuário não localizado!',
+          Message: 'Ops! Não é possível transferir para sua própria conta!',
         });
       }
     } else {
