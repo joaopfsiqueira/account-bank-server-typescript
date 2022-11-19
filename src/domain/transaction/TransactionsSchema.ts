4b83ca1296d990f260ff67414d0eb4768c9d7896
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 16:03:49 -0300

    feat: schema para userTransactions

--- src/domain/transaction/TransactionsSchema.ts
+++ src/domain/transaction/TransactionsSchema.ts
@@ -5,3 +5,7 @@ export const TransactionSchema = Joi.object({
   debitedAccount: Joi.number().required(),
   creditedUsername: Joi.string().required().min(3),
 });
+
+export const TransactionsUser = Joi.object({
+  account: Joi.number().required(),
+});
