59229fb69e6832762bf3f34444d97c64c1ac9265
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 23:10:50 -0300

    feat: limpando app

--- src/app.ts
+++ src/app.ts
@@ -23,45 +23,6 @@ app.use(logger('dev'));
 // conecta no banco
 AppDataSource.initialize()
   .then(async () => {
-    // const account = new Accounts();
-    // const account2 = new Accounts(); // nãoo preciso especificar o value, já que na entity eu seto um valor padrão.
-
-    // await AppDataSource.manager.save(account);
-    // await AppDataSource.manager.save(account2);
-    // console.log('Saved a new account with id: ' + account.id);
-
-    // const user = new Users();
-    // user.username = '@Joaopfsiqueira';
-    // user.password = '123';
-    // user.account = account;
-
-    // const user2 = new Users();
-    // user2.username = '@Romasiqueira';
-    // user2.password = '123';
-    // user2.account = account2;
-
-    // await AppDataSource.manager.save(user);
-    // await AppDataSource.manager.save(user2);
-    // console.log('Saved a new Users with id: ' + user.id);
-
-    // console.log('Loading users from the database...');
-    // const users = await AppDataSource.manager.find(Users);
-    // console.log('Loaded users: ', users);
-
-    // const transaction = new Transactions();
-    // transaction.value = 50;
-    // transaction.debitedAccount = account;
-    // transaction.creditedAccount = account2;
-
-    // await AppDataSource.manager.save(transaction);
-
-    // account.balance = account.balance - transaction.value; //debito
-    // account2.balance = account2.balance + transaction.value; // credito o problema é que acha que vai concatenar por conta do +, dito isso, criei um transformer
-    // console.log(account2.balance);
-
-    // await AppDataSource.manager.save(account2);
-    // await AppDataSource.manager.save(account);
-
     console.log(
       'Here you can setup and run express / fastify / any other framework.'
     );
