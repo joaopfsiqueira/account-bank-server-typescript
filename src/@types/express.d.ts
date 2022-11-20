39b5f402356f77711a1ca9e7771e8b7498b203a7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 23:08:14 -0300

    feat: merge na interface do express.

---
+++ src/@types/express.d.ts
@@ -0,0 +1,7 @@
+declare namespace Express {
+  //acessando o express.
+  //pegando a interface criada abaixo e dando um merge na interface do express.
+  export interface Request {
+    userId: string;
+  }
+}
