7e5567834dc4da57969ed283a1398707c4857aea
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 21:24:01 -0300

    feat: transformando em function

--- src/common/CheckBalance.ts
+++ src/common/CheckBalance.ts
@@ -1,19 +1,17 @@
 import { Accounts } from '../domain/account/AccountsEntity';
 import { AccountRepository } from '../repository/pgSQL/Repositories-pgSQL';
 
-export class CheckBalance {
-  public async CheckUserBalance(
-    debitedAccount: Accounts,
-    value: number
-  ): Promise<Boolean> {
-    const accountBalance = await AccountRepository.findOneBy({
-      id: debitedAccount.id,
-    });
+export async function CheckUserBalance(
+  debitedAccount: Accounts,
+  value: number
+): Promise<Boolean> {
+  const accountBalance = await AccountRepository.findOneBy({
+    id: debitedAccount.id,
+  });
 
-    if (accountBalance.balance < value) {
-      return false;
-    } else {
-      return true;
-    }
+  if (accountBalance.balance < value) {
+    return false;
+  } else {
+    return true;
   }
 }
