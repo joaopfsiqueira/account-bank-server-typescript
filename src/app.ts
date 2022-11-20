a544f341f82e7ea48940f5c78af62b2ee6c614ba
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 23:11:05 -0300

    feat: limpando app

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import * as logger from 'morgan';
 
 import { AppDataSource } from './data-source';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { resolve } from 'path';
 dotenv.config();
 
 // criando app
