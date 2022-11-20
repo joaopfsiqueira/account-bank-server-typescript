de6016699cecbf5e9336c80910aee89165e38112
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 23:09:07 -0300

    feat: populando req.userID

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 
+// criando interface para o retorno do "Data" mais abaixo
+interface TokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
 export default function AuthMiddleware(
   req: Request,
   res: Response,
@@ -18,7 +25,12 @@ export default function AuthMiddleware(
 
   try {
     const data = jwt.verify(token, process.env.SECRET_KEY); //comparando token depois de descripitografar o token com a nossa secret.
-    console.log(data);
+    const { id } = data as TokenPayload; // criamos uma interface para esse payload.
+
+    // foi preciso criar um tipo customizavel dentro do req do express. @types/
+    req.userId = id;
+
+    return next();
   } catch {
     return res.status(401).send('Unauthorized');
   }
