bc1255a3d8153d784a1924596b8bb6a07a794cde
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 22:33:39 -0300

    feat: removendo login controller

--- src/domain/user/UsersController.ts
+++ src/domain/user/UsersController.ts
@@ -46,17 +46,6 @@ export default class UsersControllers {
     }
   }
 
-  public async login(req: Request, res: Response): Promise<Response> {
-    const { username, password } = req.body; // associação por desestruturação.
-
-    try {
-      const login = await userService.loginUser(username, password);
-      return res.send(login);
-    } catch (error) {
-      return res.status(404).send(getErrorMessage(error));
-    }
-  }
-
   public async balance(req: Request, res: Response): Promise<Response> {
     const { username } = req.body;
 
