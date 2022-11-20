afe99cab1e8bacf486379e99b85e7712595ad1ff
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-20 00:30:19 -0300

    feat: tirando username do schema

--- src/domain/transaction/TransactionsSchema.ts
+++ src/domain/transaction/TransactionsSchema.ts
@@ -2,7 +2,6 @@ import Joi = require('joi');
 
 export const TransactionSchema = Joi.object({
   value: Joi.number().required(),
-  debitedAccount: Joi.number().required(),
   creditedUsername: Joi.string().required().min(3),
 });
 
