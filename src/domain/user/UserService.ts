8b7896f82d8b629ad37d3b71cd9850ba669095f2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 22:36:31 -0300

    feat: removendo login de service

--- src/domain/user/UserService.ts
+++ src/domain/user/UserService.ts
@@ -1,8 +1,6 @@
 import { UserRepository } from '../../repository/pgSQL/Repositories-pgSQL';
 import { Users } from './UsersEntity';
 import * as AccountService from '../account/AccountService';
-import * as Jwt from 'jsonwebtoken';
-import bcrypt = require('bcrypt');
 
 export async function findByUsername(username: string): Promise<Users> {
   return await UserRepository.findOneBy({
@@ -21,33 +19,6 @@ export async function createUser(
       const newAccount = await AccountService.createAccount();
       const newUser = new Users(username, password, newAccount);
       return await UserRepository.save(newUser);
-    }
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function loginUser(
-  username: string,
-  password: string,
-  SECRET_KEY: string
-): Promise<Object> {
-  try {
-    const loginUser = await UserRepository.findOne({
-      where: {
-        username: username,
-      },
-    });
-
-    if (!loginUser) {
-      throw new Error('Username incorreto!');
-    }
-
-    const isMatch = bcrypt.compareSync(password, loginUser.password);
-    if (isMatch) {
-      return { loginUser };
-    } else {
-      throw new Error('Senha errada!');
     }
   } catch (error) {
     throw error;
