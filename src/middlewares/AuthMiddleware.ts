3e36968dc2aa0379cd9d2484cbc64445f9938ac2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-19 22:55:46 -0300

    feat: criando auth middleware

---
+++ src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+
+export default function AuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+
+  //se não houver
+  if (!authorization) {
+    return res.sendStatus(401);
+  }
+
+  //pegando o token do request.
+  const token = authorization.replace('Bearer', '').trim();
+
+  try {
+    const data = jwt.verify(token, process.env.SECRET_KEY); //comparando token depois de descripitografar o token com a nossa secret.
+    console.log(data);
+  } catch {
+    return res.sendStatus(401);
+  }
+}
