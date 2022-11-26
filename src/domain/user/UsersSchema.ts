ca085ac876f042a7873037da2479a9be04da6e40
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-26 20:31:19 -0300

    feat: melhorando user joi

--- src/domain/user/UsersSchema.ts
+++ src/domain/user/UsersSchema.ts
@@ -8,5 +8,11 @@ export const createUserSchema = Joi.object({
       new RegExp(
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
       )
-    ),
+    )
+    .messages({
+      'string.empty': 'Senha não pode ser vazia!',
+      'any.required': 'Campo password é necessário.',
+      'string.pattern.base':
+        'Senha deve conter ao menos 8 caracteres, uma letra maíuscula, 1 caracter especial e 1 número!',
+    }),
 });
