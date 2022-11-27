e52921b6e9e9a95497a8e5d6ff26e6a333b85109
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-26 22:23:14 -0300

    feat: tratando erro com schema.

--- src/domain/transaction/TransactionsSchema.ts
+++ src/domain/transaction/TransactionsSchema.ts
@@ -1,6 +1,14 @@
 import Joi = require('joi');
 
 export const TransactionSchema = Joi.object({
-  value: Joi.number().required(),
-  creditedUsername: Joi.string().required().min(3),
+  value: Joi.number().required().messages({
+    'number.empty': 'Valor não pode ser vazio!',
+    'any.required': 'Campo valor necessário!',
+  }),
+  creditedUsername: Joi.string().required().min(3).messages({
+    'string.empty':
+      'Usuário que vai receber a transaferência não pode ser vazio!',
+    'any.required': 'Campo creditedUsername necessário',
+    'string.min': 'CreditedUsername deve ter ao menos 3 caracteres!',
+  }),
 });
